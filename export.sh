32c2da11937d6a16701014d3a73f63af8713ca15
guigui64 <guillaume.comte10@gmail.com>
2020-04-12 23:29:36 +0200

    vim: coc + update

--- export.sh
+++ export.sh
@@ -2,4 +2,5 @@
 
 git archive -o configs.zip HEAD
 git submodule --quiet foreach 'cd $toplevel; zip -ru configs.zip $sm_path'
+zip -ru configs.zip vim/pack/plugins/start/coc.nvim-release
 echo -e "Repository:\n$(git rev-parse HEAD)\nSubmodule status:\n$(git submodule status)" | zip -u configs.zip -z
--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 git submodule update --remote --merge --recursive
+cd vim/pack/plugins/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv -
+cd -
 find vim/ -name doc -exec vim -u NONE -c "helptags {}" -c q \;

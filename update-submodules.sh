c6b9dd6e31ffad452753b35ed3c6aac0d93eec2e
Guillaume Comte <guillaume.comte@airbus.com>
2021-02-15 10:51:53 +0100

    submodule update --init

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git submodule update --remote --merge --recursive
+git submodule update --init --remote --merge --recursive
 vim -c "CocUpdateSync | q"
 find vim/ -name doc -exec vim -u NONE -c "helptags {}" -c q \;

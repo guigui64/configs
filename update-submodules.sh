2de8dcbaec3df292a58398930505b4589bbd70dd
Guillaume Comte <guillaume.comte@airbus.com>
2021-06-24 13:43:06 +0200

    no more vim in update-submodules script

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git submodule update --remote --merge --recursive
-vim -c "CocUpdateSync | q"
-find vim/ -name doc -exec vim -u NONE -c "helptags {}" -c q \;
+# vim -c "CocUpdateSync | q"
+# find vim/ -name doc -exec vim -u NONE -c "helptags {}" -c q \;
